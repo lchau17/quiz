f96210eb8664ba1b4367b19861fdfd806255e5eb
aamayzingg <maychau9@gmail.com>
2021-03-21 03:09:41 -0700

    test

--- js/app.js
+++ js/app.js
@@ -21,76 +21,76 @@ http.createServer(function (request, response) {
     })
     request.on('end', () => {
         console.log(data); // 'Buy the milk'
-    })
-
-    console.log(request.method);
-    const reqUrl = new URL(request.url, 'https://aamayzingg.com/COMP4537/labs/quiz/questions');
-    if (request.method == "POST") {
-        console.log("POST");
-        data = JSON.parse(data);
-        console.log(data);
-        const id = reqUrl.searchParams.get('id');
-        const question = reqUrl.searchParams.get('question');
-        let sql = `INSERT INTO questions(id, question) values ('${id}', ${question})`;
-        try {
-            con.query(sql, function (err, result) {
-                if (err) 
-                {
-                    throw err;
-                } else {
-                    const options = reqUrl.searchParams.get('options');
-                    for (let i = 0; i < options.length; i++) {
-                        let sql = `INSERT INTO answers(question_id, option_id, answer, is_answer) values ('${id}', ${options[i]['option_id']}, ${options[i]['answer']}, ${options[i]['is_answer']})`;
-                        con.query(sql, function (err, result) {
-                            if (err) {
-                                throw err;
-                            } else {
-                                response.writeHead(200, {'Content-type': 'text/plain', "Access-Control-Allow-Origin": "*"});
-                                response.end();
-                            }
-                        });
-                    }
-                }
-            })
-        } catch (err) {
-            console.log(err);
-        }        
         
-    } else if (request.method == "GET") {
-        let questions = []
-        let questionSql = "SELECT * FROM questions q";
-        con.query(questionSql, function (err, rows) {
-            if (!err){
-                if (rows.length) {
-                    for (let i = 0; i < rows.length; i++) {
-                        let question = {}
-                        question['id'] = rows[i].id
-                        question['question'] = rows[i].question
-                        let answerSql = `SELECT * FROM answers a where question_id = ${rows[i].id}`;
-                        con.query(answerSql, question, function (err, result) {
-                            if (!err){
-                                if (result.length) {
-                                    question['options'] = result;
-                                    questions.push(question);
-                                    console.log(questions);
-                                    if (i == rows.length - 1) {
-                                        const resultStr = JSON.stringify(questions);
-                                        if (err) throw err;
-                                        response.writeHead(200, {'Content-type': 'text/plain', "Access-Control-Allow-Origin": "*"});
-                                        response.write(resultStr);
-                                        response.end();
-                                    }
+        console.log(request.method);
+        const reqUrl = new URL(request.url, 'https://aamayzingg.com/COMP4537/labs/quiz/questions');
+        if (request.method == "POST") {
+            console.log("POST");
+            data = JSON.parse(data);
+            console.log(data);
+            const id = reqUrl.searchParams.get('id');
+            const question = reqUrl.searchParams.get('question');
+            let sql = `INSERT INTO questions(id, question) values ('${id}', ${question})`;
+            try {
+                con.query(sql, function (err, result) {
+                    if (err) 
+                    {
+                        throw err;
+                    } else {
+                        const options = reqUrl.searchParams.get('options');
+                        for (let i = 0; i < options.length; i++) {
+                            let sql = `INSERT INTO answers(question_id, option_id, answer, is_answer) values ('${id}', ${options[i]['option_id']}, ${options[i]['answer']}, ${options[i]['is_answer']})`;
+                            con.query(sql, function (err, result) {
+                                if (err) {
+                                    throw err;
+                                } else {
+                                    response.writeHead(200, {'Content-type': 'text/plain', "Access-Control-Allow-Origin": "*"});
+                                    response.end();
                                 }
-                            } else {
-                                throw err;
-                            }
-                        })
+                            });
+                        }
                     }
-                    
+                })
+            } catch (err) {
+                console.log(err);
+            }        
+            
+        } else if (request.method == "GET") {
+            let questions = []
+            let questionSql = "SELECT * FROM questions q";
+            con.query(questionSql, function (err, rows) {
+                if (!err){
+                    if (rows.length) {
+                        for (let i = 0; i < rows.length; i++) {
+                            let question = {}
+                            question['id'] = rows[i].id
+                            question['question'] = rows[i].question
+                            let answerSql = `SELECT * FROM answers a where question_id = ${rows[i].id}`;
+                            con.query(answerSql, question, function (err, result) {
+                                if (!err){
+                                    if (result.length) {
+                                        question['options'] = result;
+                                        questions.push(question);
+                                        console.log(questions);
+                                        if (i == rows.length - 1) {
+                                            const resultStr = JSON.stringify(questions);
+                                            if (err) throw err;
+                                            response.writeHead(200, {'Content-type': 'text/plain', "Access-Control-Allow-Origin": "*"});
+                                            response.write(resultStr);
+                                            response.end();
+                                        }
+                                    }
+                                } else {
+                                    throw err;
+                                }
+                            })
+                        }
+                        
+                    }
                 }
-            }
-        });
-    }}   
-).listen(8070);
+            });
+        }
+    })
+}).listen(8070);
 console.log('listening...');
  
