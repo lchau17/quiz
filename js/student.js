c6617cb0a286d9ab58ca81f8e1bf1638f1a07e4e
aamayzingg <maychau9@gmail.com>
2021-03-21 12:53:55 -0700

    update stdent page

--- js/student.js
+++ js/student.js
@@ -3,7 +3,17 @@ import { createContentDiv, createButton } from './doc.js';
 import { getLocalStorage } from './doc.js';
 
 
+const numOfOptions = 4;
+
+
 function onSubmitClick(questionsList){
+    let rightAnwser = []
+    for (let i = 0; i < questionsList; i++){
+        rightAnswer.push(questionsList[i]['options'].filter(o => o['is_answer'] === true))
+
+    }
+    console.log(rightAnwser)
+
     let correctAnswer = 0;
     let selectedOptions = document.querySelectorAll("input:checked");
     let answers = Array.from(document.querySelectorAll(".correct"));
@@ -33,30 +43,62 @@ function onSubmitClick(questionsList){
     contentDiv.appendChild(result);
 }
 
-function createStudentPage(){
 
-    const questionsList = getLocalStorage("questions");
+createStudentQuestion(question) {
+    let questionDiv = createQuestionDiv(question['id']);
+    let questionText = document.createElement("p");
+    questionText.innerText = question['question'];
+    questionDiv.appendChild(questionText);
+    for (let i = 0; i < numOfOptions; i++){
+        let radiobox = document.createElement('input');
+        radiobox.type = "radio";
+        radiobox.id = `q${question['id']}-choice${i+1}`;
+        radiobox.name =  `q${question['id']}`;
 
-    
-    if (questionsList.length === 0 || questionsList[0] === null){
-        document.body.innerHTML = "No quiz question found"
-    } else {
-        let contentDiv = createContentDiv();
-        Question.createStudentQuestionList(questionsList);
+        let label = document.createElement('label');
+        label.htmlFor = radiobox.id;
+        label.innerHTML = this.options[i];
+        // if (this.options[i] === this.answer){
+        //     label.classList.add("correct");
+        // } else{
+        //     label.classList.add("incorrent");
+        // }
 
-        let submitBtn = createButton('Submit', function(){
-            onSubmitClick(questionsList)
-        });
-        contentDiv.appendChild(submitBtn);
+        let br = document.createElement("br");
+        
+        questionDiv.appendChild(br);
+        questionDiv.appendChild(radiobox);
+        questionDiv.appendChild(label);
+        questionDiv.appendChild(br);
     }
-
-    let backBtn = createButton("Back", function(){
-        window.history.back();
-    })
-    document.body.appendChild(backBtn);
 }
 
-createStudentPage();
 
 
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://aamayzingg.com/COMP4537/labs/quiz/questions/", true);
+xhttp.send();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+        console.log(this.responseText);
+        const data = JSON.parse(this.responseText);
+        numOfQuestions = data.length;
+        if (numOfQuestions === 0){
+            document.body.innerHTML = "No quiz question found"
+            return;
+        }
+        contentDiv = createContentDiv();
 
+
+        for (let i = 0; i < data.length; i ++) {
+            createStudentQuestion(data[i]);
+        }   
+        let submitBtn = createButton('Submit', function(){
+            onSubmitClick(data)
+        });
+        contentDiv.appendChild(submitBtn);
+        let backBtn = createButton("Back", function(){
+            window.history.back();
+        })
+        document.body.appendChild(backBtn);
+    }};
